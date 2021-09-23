37ab53b36eeb33523db87beb23d25c004ad3b890
Iyadi Cyril <iyadicy@gmail.com>
2021-09-23 08:15:54 +0100

    Update index.js
    add import statement for list.js
    add save to local storage method

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './style.css';
+import { toggleCompleteTask, strikeText } from './list.js';
 
 class TodoList {
   constructor() {
@@ -16,13 +17,20 @@ class TodoList {
     ];
   }
 
+  saveToLocalStorage() {
+    localStorage.setItem('Tasks', JSON.stringify(this.ArrayOfTasks));
+  }
+
   loadTasks() {
     const divHolder = document.getElementById('list-holder');
+
+    if (localStorage.getItem('Tasks') !== null) {
+      this.ArrayOfTasks = JSON.parse(localStorage.getItem('Tasks'));
+    }
 
     this.ArrayOfTasks.forEach((item) => {
       const ul = document.createElement('ul');
       ul.className = 'todo-list-ul';
-      ul.id = 'list-ul';
 
       const li = document.createElement('li');
       li.className = 'todo-list-ul-li';
@@ -34,11 +42,20 @@ class TodoList {
       checkInput.checked = item.completed;
       checkInput.className = 'todo-list-ul-li-box';
 
+      checkInput.addEventListener('change', (e) => {
+        const textInput = e.target.parentNode.lastChild;
+        toggleCompleteTask(textInput); /* eslint-disable-line no-undef */
+        item.completed = checkInput.checked;
+        this.saveToLocalStorage();
+      });
+
       const textInput = document.createElement('input');
       textInput.type = 'text';
       textInput.value = item.description;
       textInput.readOnly = true;
       textInput.className = 'todo-list-ul-li-input';
+
+      strikeText(item.completed, textInput);
 
       styleDiv.appendChild(checkInput);
       styleDiv.appendChild(textInput);
