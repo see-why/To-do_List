1bbb54c4851c52b8e8846228b16a23195d66f8ff
Iyadi Cyril <iyadicy@gmail.com>
2021-09-30 15:28:53 +0100

    Update tasks.test.js
    add new test for update task description and update completed status

--- src/tasks.test.js
+++ src/tasks.test.js
@@ -1,4 +1,5 @@
-import { addTask, deleteTask } from './tasks.js';
+import { addTask, deleteTask, updateTask } from './tasks.js';
+import { updateStatus } from './list.js';
 
 const task1 = {
   index: 0,
@@ -45,13 +46,55 @@ describe('delete tasks', () => {
   test('delete task', () => {
     const currentList = deleteTask(taskList, task4);
 
-    expect(currentList).toStrictEqual([task1, task2, task3]);
+    expect(currentList).toHaveLength(3);
   });
 
   test('delete two tasks', () => {
-    let currentList = deleteTask(taskList, task4);
-    currentList = deleteTask(currentList, task3);
+    const List1 = deleteTask(taskList, task4);
+    const List2 = deleteTask(List1, task3);
 
-    expect(currentList).toStrictEqual([task1, task2]);
+    expect(List2).toHaveLength(2);
+  });
+});
+
+describe('update tasks', () => {
+  const newtask4 = {
+    index: 3,
+    description: 'buy guns',
+    completed: false,
+  };
+
+  const newtask3 = {
+    index: 2,
+    description: 'singing',
+    completed: false,
+  };
+
+  test('update task', () => {
+    const List = updateTask(taskList, newtask4);
+
+    expect(List[3].description).toBe(newtask4.description);
+  });
+
+  test('update two tasks', () => {
+    let List = updateTask(taskList, newtask4);
+    List = updateTask(List, newtask3);
+
+    expect(List[3].description).toBe(newtask4.description);
+    expect(List[2].description).toBe(newtask3.description);
+  });
+});
+
+describe('update task complete status', () => {
+  test('update status to true', () => {
+    const newList = updateStatus(taskList, '2', true);
+
+    expect(newList[1].completed).toBe(true);
+  });
+
+  test('update status to false', () => {
+    const newList = updateStatus(taskList, '2', false);
+
+    expect(newList[1].completed).toBe(false);
   });
 });
