92dfb75bf5c0230a161c74ea13c991ea3878840d
Iyadi Cyril <iyadicy@gmail.com>
2021-09-30 12:47:24 +0100

    Update list.js
    add export statement at the bottom of the file

--- src/list.js
+++ src/list.js
@@ -1,4 +1,4 @@
-export const toggleCompleteTask = (className) => {
+const toggleCompleteTask = (className) => {
   if (className.style.textDecoration === 'line-through') {
     className.style.textDecoration = 'none';
   } else {
@@ -6,8 +6,18 @@ export const toggleCompleteTask = (className) => {
   }
 };
 
-export const strikeText = (checked, className) => {
+const strikeText = (checked, className) => {
   if (checked) {
     toggleCompleteTask(className);
   }
+};
+
+const updateStatus = (array, itemId, checked) => {
+  const key = array.findIndex((x) => x.index === itemId);
+  array[key].completed = checked;
+  return array;
+};
+
+export {
+  toggleCompleteTask, strikeText, updateStatus,
 };
