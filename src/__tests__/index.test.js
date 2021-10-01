e897a5dfab4d5b905ebd410c8836b556cccd4f5f
Iyadi Cyril <iyadicy@gmail.com>
2021-10-01 09:26:41 +0100

    Update index.test.js
    add test for add two new tasks into local storage

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -9,4 +9,15 @@ describe('Add to-do', () => {
 
     expect(localStorageMock.getItem('tasks')).toHaveLength(1);
   });
+
+  test('Local storage should be updated after adding two items', () => {
+    document.body.innerHTML = '<input id="add-new-task" value="dance for me">';
+    const newTodoList = new TodoList();
+    newTodoList.addInputTagEvent();
+
+    document.body.innerHTML = '<input id="add-new-task" value="jump rope">';
+    newTodoList.addInputTagEvent();
+
+    expect(localStorageMock.getItem('tasks')).toHaveLength(2);
+  });
 });
